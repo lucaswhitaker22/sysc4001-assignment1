b909af50894a81fc843bfaa0cd60b955f3d115f5
Lucas Whitaker <lucaswhitaker22@gmail.com>
2022-10-11 14:40:08 -0400

    Main

--- client.h
+++ client.h
@@ -6,10 +6,11 @@
 #include <limits.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#define SERVER_FIFO_NAME “/tmp/serv_fifo”
-#define CLIENT_FIFO_NAME “/tmp/cli_%d_fifo”
+#define SERVER_FIFO_NAME “/ tmp / serv_fifo”
+#define CLIENT_FIFO_NAME “/ tmp / cli_ % d_fifo”
 #define BUFFER_SIZE 20
-struct data_to_pass_st {
-pid_t client_pid;
-char some_data[BUFFER_SIZE - 1];
+struct data_to_pass_st
+{
+    pid_t client_pid;
+    char some_data[BUFFER_SIZE - 1];
 };
